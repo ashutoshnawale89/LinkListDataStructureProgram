d47a4d85d8c29e1039473c6ef27ae54ad95e4eba
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-11 10:22:31 +0530

    Added: Middle Between Two

--- src/com/linkedlist/program/LinkedListProgrm.java
+++ src/com/linkedlist/program/LinkedListProgrm.java
@@ -1,46 +1,78 @@
 package com.linkedlist.program;
 
+import com.Linklists.Assignment.LinkListUC4;
+import com.Linklists.Assignment.LinkListUC4.Node;
+
 public class LinkedListProgrm {
-	Node head;
-
+	   Node head; 
+       int size;
+	
+       LinkedListProgrm() {
+    	   this.size=3;
+       }
+       
 	class Node {
-		int data;
-		Node next;
-
-		Node (int data) {    //  CONSTRUCTOR 
-			this.data=data;
-			this.next=null;
-		}
+     int data;
+     Node next;
+    
+     Node (int data) {    //  CONSTRUCTOR 
+     	this.data=data;
+     	this.next=null;
+     }
 	}
-
 	// ADD - FIRST POSITION
-	public void addfirst(int data) {
-		Node newNode=new Node(data);
-		newNode.next = head;
-		head = newNode;
-		return;
-	}
-	// PRINT METHOD
+		public void addfirst(int data) {
+			Node newNode=new Node(data);
+			newNode.next = head;
+			head = newNode;
+			return;
+		}
+		 // PRINT METHOD
 	public void printlist() {
-		if (head == null) {
-			System.out.println("List is Empty");
+	     Node currNode = head;
+			while (currNode != null)  {
+				System.out.print(currNode.data +"->" );
+				currNode = currNode.next;
+			}
+			System.out.println("null");
+			
+			}
+	// ADD - MIDDLE POSITION 
+	public void addMiddle(int num,int data) {
+		if (num > size || num < 0 ) {
+			System.out.println("The input Number is Error");
 		}
-		Node currNode = head;
-		while (currNode != null)  {
-			System.out.print(currNode.data +"->" );
-			currNode = currNode.next;
+		
+		Node newnode=new Node(data);
+		if (head == null || num == 0) {
+			newnode.next=head;
+			head=newnode;	
 		}
-		System.out.println("null");
-
+		Node currnode=head;
+		
+		for (int i=1; i<num; i++) {
+              if (i==num) {
+				
+				Node nextnode=currnode.next;
+				currnode.next=newnode;
+				newnode.next=nextnode;
+              }			
+		}
+		System.out.println(currnode.data+"->"+newnode.data +"->"+currnode.next.data+"->");
 	}
+	
 
-	public static void main(String[] args) {
-		LinkedListProgrm list=new LinkedListProgrm();
-		list.addfirst(56);
-		list.addfirst(30);
-		list.addfirst(70);
-		list.printlist();
-
-	}
-
+		
+   public static void main(String[] args) {
+ 	  
+	   LinkedListProgrm list=new LinkedListProgrm();
+			list.addfirst(56);
+			list.addfirst(70);
+			list.printlist();
+			list.addMiddle(2,30);
+     	
+			
+			
+			
+		}
 }
