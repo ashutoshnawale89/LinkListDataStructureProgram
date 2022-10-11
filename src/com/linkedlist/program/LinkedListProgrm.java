06d72cfc5b6016584d15f24562843000ffdf1308
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-11 10:44:06 +0530

    Added: Delete First Program

--- src/com/linkedlist/program/LinkedListProgrm.java
+++ src/com/linkedlist/program/LinkedListProgrm.java
@@ -24,6 +24,16 @@ class Node {
 		head = newNode;
 		return;
 	}
+	// Delete First Method 
+	public void deletefirst() {
+		if (head==null) {
+			System.out.println("The list is Empty");
+		}
+		else {
+			head=head.next;
+		}
+	}
+		
 	 // PRINT METHOD
 public void printlist() {
      Node currNode = head;
@@ -34,39 +44,15 @@ public void printlist() {
 		System.out.println("null");
 		
 		}
-// ADD - MIDDLE POSITION 
-public void addMiddle(int num,int data) {
-	if (num > size || num < 0 ) {
-		System.out.println("The input Number is Error");
-	}
-	
-	Node newnode=new Node(data);
-	if (head == null || num == 0) {
-		newnode.next=head;
-		head=newnode;	
-	}
-	Node currnode=head;
-	
-	for (int i=1; i<num; i++) {
-             if (i==num) {
-			
-			Node nextnode=currnode.next;
-			currnode.next=newnode;
-			newnode.next=nextnode;
-             }			
-	}
-	System.out.println(currnode.data+"->"+newnode.data +"->"+currnode.next.data+"->");
-}
-
-
 	
   public static void main(String[] args) {
 	  
 	  LinkedListProgrm list=new LinkedListProgrm();
 		list.addfirst(56);
+		list.addfirst(30);
 		list.addfirst(70);
 		list.printlist();
-		list.addMiddle(2,30);
-		
+		list.deletefirst();
+		list.printlist();
 	}
 }
