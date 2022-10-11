901afb5ef102cbcc4daafc42b2747683d0024982
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-11 10:17:04 +0530

    Added: Simple LInkedList

--- src/com/linkedlist/program/LinkedListProgrm.java
+++ src/com/linkedlist/program/LinkedListProgrm.java
@@ -1,7 +1,46 @@
 package com.linkedlist.program;
 
 public class LinkedListProgrm {
-	public static void main(String[] args) {
-		System.out.println("Welcome  LinkedList Program");
+	Node head;
+
+	class Node {
+		int data;
+		Node next;
+
+		Node (int data) {    //  CONSTRUCTOR 
+			this.data=data;
+			this.next=null;
+		}
 	}
+
+	// ADD - FIRST POSITION
+	public void addfirst(int data) {
+		Node newNode=new Node(data);
+		newNode.next = head;
+		head = newNode;
+		return;
+	}
+	// PRINT METHOD
+	public void printlist() {
+		if (head == null) {
+			System.out.println("List is Empty");
+		}
+		Node currNode = head;
+		while (currNode != null)  {
+			System.out.print(currNode.data +"->" );
+			currNode = currNode.next;
+		}
+		System.out.println("null");
+
+	}
+
+	public static void main(String[] args) {
+		LinkedListProgrm list=new LinkedListProgrm();
+		list.addfirst(56);
+		list.addfirst(30);
+		list.addfirst(70);
+		list.printlist();
+
+	}
+
 }
