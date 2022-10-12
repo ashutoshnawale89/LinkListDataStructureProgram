0f9f5161699f36fcd8550e79dd8ceb534ef10419
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-10-12 11:14:46 +0530

    Added: Create Stack Method and Add Push Method

--- src/com/stack/program/StackProgram.java
+++ src/com/stack/program/StackProgram.java
@@ -2,7 +2,39 @@ package com.stack.program;
 
 public class StackProgram {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Stack And Queue Program");
+	public Node head;
+		// FOR NODE CLASS
+	   class Node  {
+			int data;
+			Node next;
+			// Constructor 
+			Node (int data){
+				this.data=data;
+				this.next=null;
+			}
+		}
+	   // Push Method
+			public void push(int data) {
+				Node newNode=new Node(data);
+				if (head == null) {
+					head=newNode; 
+					System.out.print(head.data+"->");
+				}
+				else {
+					newNode.next = head;
+					head = newNode;
+					System.out.print(head.data+"->");
+
+				}  
+			}
+		
+		public static void main(String[] args) {
+			StackProgram obj=new StackProgram();
+			obj.push(70);
+			obj.push(30);
+			obj.push(56);
+			System.out.print("null");
+		}
 	}
-}
+
+
